d497cbbf182c51737394809c56cf810686eb5609
Laurent Steff <laurent.steff@inria.fr>
2022-05-16 13:55:27 +0200

    wip

--- second.cpp
+++ second.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
@@ -9,5 +11,20 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    return 0;
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+      std::cout << "Failed to create GLFW window" << std::endl;
+      glfwTerminate();
+      return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+      std::cout << "Failed to initialize GLAD" << std::endl;
+      return -1;
+  }
+
+  return 0;
 }
