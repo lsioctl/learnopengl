febbb17e7f5e5f385dfc3fc72f38e9c833feb6dc
Laurent Steff <laurent.steff@inria.fr>
2022-07-01 13:00:39 +0200

    working 3d

---
+++ VAO2.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+#include "glad/glad.h"
+
+/**
+ * VAO2 created to avoid breaking existing API and changes are:
+ * - no EBO
+ * - the vertices array does only include vertices and textures (so strid of 5)
+ * 
+ * TODO: find a better OOD, I will wait until other examples
+ */
+
+struct VAO2 final {
+  GLuint id;
+  // in C++ C array are passed by pointer
+  VAO2(const std::vector<float>& vertices);
+  void bind();
+  void unbind();
+};
